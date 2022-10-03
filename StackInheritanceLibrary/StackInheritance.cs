d51154818cc98877148a43025501aab5bbf14df5
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-10-03 10:38:15 -0600

    Add project files.

---
+++ StackInheritanceLibrary/StackInheritance.cs
@@ -0,0 +1,26 @@
+﻿using LinkedListLibrary;
+using System.Collections.Generic;
+
+namespace StackInheritanceLibrary
+{
+    public class StackInheritance : List
+    {
+        // Pass name "stack" to List constructor
+        public StackInheritance() : base("stack") { }
+
+        // Place ditem from dataValue at top of stack by inserting
+        // dataValue at front of linked list
+        public void Push(object dataValue)
+        {
+            InsertAtFront(dataValue);
+        }
+
+        // Remove item from top of stack by removing
+        // item at front of linked list
+
+        public object Pop()
+        {
+            return RemoveFromFront();
+        }
+    }
+}
